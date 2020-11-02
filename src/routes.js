09fe24778d173637486c2b083491319ca6266b6a
katacek <56041262+katacek@users.noreply.github.com>
2020-11-02 21:02:08 +0100

    fix

--- src/routes.js
+++ src/routes.js
@@ -1,20 +1,25 @@
 const Apify = require('apify');
 const urlClass = require('url');
 
-//const { utils: { log } } = Apify;
+const { utils: { log } } = Apify;
 
-exports.handleStart = async ({ $ }) =>
+// at this point, the main page is already loaded in $
+exports.handleStart = async ({ $, request }) =>
 {
     const requestQueue = await Apify.openRequestQueue();
     //start page, add all categories links to requestQueue
-    const links = $('div#box-categories').find('li').map(function ()
+    //const links = $( "a[data-type='subcategory']" ).map(function ()
+    //{ return $(this).attr('href'); }).get();
+    const links = $('div#box-categories').find('li').find('h2').map(function ()
     { return $(this).find('a').attr('href'); }).get();
-    console.log(links);
+    
     for (let link of links)
     {   
-        await requestQueue.addRequest({
-        url: link,
-         userData: { label: 'LIST' },
+        // request is an object, setting url to link and in userdata, setting new dictionary label: LIST
+        // it is me who is setting the label value, just using it for making the crawler fcn more clear
+         await requestQueue.addRequest({
+            url: link,
+             userData: { label: 'LIST' },
         });
     }
 
@@ -25,7 +30,8 @@ exports.handleList = async ({ request, $ }) =>
     const requestQueue = await Apify.openRequestQueue();
     //add detail pages of all products on the page to requestQueue
     const links = $( ".product-container" ).map(function ()
-    { return $(this).find('a').attr('href'); }).get()
+    { return $(this).find('a').attr('href'); }).get();
+    
     for (let link of links)
     {
         const absoluteLink = urlClass.resolve(request.url, link);
@@ -48,10 +54,21 @@ exports.handleList = async ({ request, $ }) =>
         });
     }
 
-    
+    //add next page to requestQueue
+    let nextLink = $('a.next').attr('href');
+    if (!nextLink) nextLink = $('.c-pagination__button').attr('href');
+    if (nextLink)
+    {
+        const baseUrl = request.url.replace(/\?f=\d+/, '');
+        const nextUrl = urlClass.resolve(baseUrl, nextLink);
+        requestQueue.addRequest({ url: nextUrl, userData: { label: 'LIST' } });
+    }
+    else
+    {
+        log.info(request.url + ' finish!!');
+    }
+
 };
-
-
 exports.handleDetail = async ({ request, $ }) =>
 {
     const requestQueue = await Apify.openRequestQueue();
@@ -66,7 +83,8 @@ exports.handleDetail = async ({ request, $ }) =>
 
     result.currentPrice = parseInt($("span[itemprop='price']").text().replace(' ',''));
 
-    if(!result.currentPrice)
+   
+     if(!result.currentPrice)
     {
         const price = $('meta[name="gtm:ecommerce:detail:products"]').attr('content').split('\"priceMin\": ')[1];
         if(price)
@@ -76,17 +94,7 @@ exports.handleDetail = async ({ request, $ }) =>
             result.currentPriceNote = "Price from metadata - lowest"
         }
     }
-
-
-    if(!result.itemName || !result.currentPrice || result.currentPrice == NaN)
-    {
-        await Apify.setValue('HeurekaBadPage', 
-            {itemName: result.itemName,
-            price: result.currentPrice,
-            tag: $('meta[name="gtm:ecommerce:detail:products"]').attr('content'),
-            pageSource: $.html()});
-        
-    }
+    
     result.breadcrumb = $('#breadcrumbs').text().trim().split('Heureka.cz » ')[1]
     result.currency = "CZK";
     if ($("div[class='top-ico gtm-header-link'] span").text() === "Top")
@@ -117,20 +125,7 @@ exports.handleDetail = async ({ request, $ }) =>
             {
                 label: 'DETAIL-SPECIFIKACE',
                 result: result
-            },
-            headers: {
-                "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
-                "accept-language": "en-US,en;q=0.9",
-                "sec-fetch-dest": "document",
-                "sec-fetch-mode": "navigate",
-                "sec-fetch-site": "none",
-                "sec-fetch-user": "?1",
-                "upgrade-insecure-requests": "1"
-            },
-            "referrerPolicy": "strict-origin-when-cross-origin",
-            "body": null,
-            "method": "GET",
-            "mode": "cors",
+            }
         });
     
 };
@@ -139,22 +134,17 @@ exports.handleDetailSpecifikace = async ({ request, $ }) =>
 {
     const requestQueue = await Apify.openRequestQueue();
     let result = request.userData.result;
-    let spec = $('div#full-product-description').text().trim();
-    if (spec){
-        result.specification = spec
-    } else {
-        result.specification =  $('div .product-body__specification__short-tail-desc__perex').text().trim()
-    }
+    result.specifikace = $('div#full-product-description').text();
 
     const paramTableRows = $(".product-body__specification__params__table tr").get();
-    result.parameters = [];
+    result.parametry = [];
     
     for (i = 1; i < paramTableRows.length; i++)
     {
         let parametr = {};
         let name = $("span[id*=param-name]", paramTableRows[i]).text();
         parametr[name] = $("span[id*=param-value]", paramTableRows[i]).text();
-        result.parameters.push(parametr);
+        result.parametry.push(parametr);
           
     }
 
@@ -168,20 +158,7 @@ exports.handleDetailSpecifikace = async ({ request, $ }) =>
             {
                 label: 'DETAIL-REVIEW',
                 result: result
-            },
-            headers: {
-                "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
-                "accept-language": "en-US,en;q=0.9",
-                "sec-fetch-dest": "document",
-                "sec-fetch-mode": "navigate",
-                "sec-fetch-site": "none",
-                "sec-fetch-user": "?1",
-                "upgrade-insecure-requests": "1"
-            },
-            "referrerPolicy": "strict-origin-when-cross-origin",
-            "body": null,
-            "method": "GET",
-            "mode": "cors",
+            }
         });
     };
 
@@ -189,32 +166,57 @@ exports.handleDetailSpecifikace = async ({ request, $ }) =>
     exports.handleDetailReview = async ({ request, $ }) =>
     {
         const requestQueue = await Apify.openRequestQueue();
-        let result = request.userData.result;
-        result.recommendedByNumber = $(".recommendation span").text();
-        const notRecommended = $(".no-recommendation span").text();
-        if (notRecommended) {
-            result.notRecommendedByNumber = $(".no-recommendation span").text();
-        }
-        result.overallReviewPecentage = parseInt($("div .heureka-rank-wide span.big").text());
-        const overallReview = $(".starsReviews li").text().replace("Zobrazit:", "").split("|").map(x => x.trim());
-        result.overallReviewNumber = {}
-        overallReview.forEach(x => 
-            result.overallReviewNumber[x.split(" ")[0]] = parseInt(x.split("(")[1])
-            );
-    
-        const reviewTableRows = $(".rating-table tr").get();
-        result.reviewStars = {};
-        
-        for (i = 0; i < reviewTableRows.length; i++)
+        result = request.userData.result;
+
+        if (request.userData.label === 'DETAIL-REVIEW')
         {
-            let percentage = parseInt($(".percentage", reviewTableRows[i]).text());
-            result.reviewStars[5-i] = percentage;
-                
-        }
-        
-        await Apify.pushData(result)
-};
-
-
-
+            const requestQueue = await Apify.openRequestQueue();
+            let result = request.userData.result;
+            result.recommendedByNumber = $(".recommendation span").text();
+            const notRecommended = $(".no-recommendation span").text();
+            if (notRecommended)
+            {
+                result.notRecommendedByNumber = $(".no-recommendation span").text();
+            }
+            result.overallReviewPecentage = parseInt($("div .heureka-rank-wide span.big").text());
     
+            const reviewTableRows = $(".rating-table tr").get();
+            result.reviewStars = {};
+        
+            for (i = 0; i < reviewTableRows.length; i++)
+            {
+                let percentage = parseInt($(".percentage", reviewTableRows[i]).text());
+                result.reviewStars[5 - i] = percentage;
+                
+            }
+        }
+         //reviews
+        const reviewDivs = $('div.review');
+        if (result.reviews == undefined)
+        {
+            result.reviews = [];
+        }
+       
+        for (i = 0; i < reviewDivs.length; i++)
+        {
+            let review = {};
+
+            review.percentage = parseInt($('.eval', reviewDivs[i]).text());
+            review.text = $('.revtext p',reviewDivs[i]).text();
+            review.plusText = $('div.plus li',reviewDivs[i]).map(function () { return $(this).text(); }).get();
+            review.minusText = $('div.minus li',reviewDivs[i]).map(function () { return $(this).text(); }).get();
+            result.reviews.push(review);
+        }
+
+        const nextLink = $('a.next').attr('href');
+        if (nextLink)
+        {
+            const baseUrl = request.url.replace(/\?f=\d+/, '');
+            const nextUrl = urlClass.resolve(baseUrl, nextLink);
+            requestQueue.addRequest({ url: nextUrl, userData: { label: 'DETAIL-REVIEW-NEXTPAGE', result : result } });
+        }
+        else
+        {
+            await Apify.pushData(result)
+        }
+};
